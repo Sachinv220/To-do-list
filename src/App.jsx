7034f652eadbbe5f7699669355a43bbc647e6283
Sachin <vsachin220@gmail.com>
2022-12-09 13:04:21 +0530

    using colorModeScript instead of changing theme  manually

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Customise from "./pages/Customise";
 import { Route, Routes } from "react-router-dom";
@@ -8,11 +8,6 @@ import TodoPage from "./pages/TodoPage";
 function App() {
   const [todo, setTodo] = useState([]);
 
-  useEffect(() => {
-    if (localStorage.getItem("chakra-ui-color-mode") === "light") {
-      localStorage.setItem("chakra-ui-color-mode", "dark");
-    }
-  }, [])
   return (
     <React.Fragment>
       <Sidebar />
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
+    <ColorModeScript initialColorMode="dark" />
     <BrowserRouter>
       <App />
     </BrowserRouter>
