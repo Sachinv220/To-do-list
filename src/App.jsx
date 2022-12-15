d3fb92daec6c4399c5ed1c04f5ff17e46a117884
Sachin <vsachin220@gmail.com>
2022-12-15 12:35:41 +0530

    Lazy loading navbars

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from "react";
-import Sidebar from "./components/Sidebar";
 import Customise from "./pages/Customise";
 import { Route, Routes } from "react-router-dom";
+import { useMediaQuery } from "@chakra-ui/react";
 import Http404 from "./pages/Http404";
 import TodoPage from "./pages/TodoPage";
+const Sidebar = React.lazy(() => import("./components/Sidebar"));
+const BottomBar = React.lazy(() => import("./components/BottomBar"));
 
 function App() {
   const [todo, setTodo] = useState([]);
+  const [isSmallerThan850] = useMediaQuery("(max-width: 850px)");
 
   useEffect(() => {
     if (localStorage.getItem("chakra-ui-color-mode") === "light") {
       localStorage.setItem("chakra-ui-color-mode", "dark");
     }
-  }, [])
+  }, []);
 
   return (
     <React.Fragment>
-      <Sidebar />
+      <React.Suspense
+        fallback={<div>wait a second loading</div>}
+        children={isSmallerThan850 ? <BottomBar /> : <Sidebar />}
+      />
+
       <Routes>
         <Route
           path="/to-do-list"
