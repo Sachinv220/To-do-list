efbcf35bc69a58b794016a64daf0303049b34509
Sachin <vsachin220@gmail.com>
2022-12-20 17:13:10 +0530

    navbar with background blur

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,25 @@
-import { HStack , Button } from "@chakra-ui/react";
+import { Flex, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-const BottomBar = () => {
+const NavBar = () => {
   return (
-    <HStack top={0} position="fixed" bg="rgb(0, 0, 0, 0.1)">
+    <Flex
+      gap={10}
+      position="fixed"
+      bg="rgb(0, 0, 0, 0.2)"
+      width="100%"
+      backdropFilter="blur(16px)"
+      zIndex={10}
+      boxShadow="0 8px 32px 0 rgba( 0,0,0,);"
+    >
       <Link to="/to-do-list">
         <Button bg="transparent">Tasks</Button>
       </Link>
       <Link to="/customise">
         <Button bg="transparent">Customise</Button>
       </Link>
-    </HStack>
+    </Flex>
   );
 };
 
-export default BottomBar;
+export default NavBar;
