431d66aadb06dcac65522fc594435b60c5a051f8
Sachin <vsachin220@gmail.com>
2022-12-27 23:17:39 +0530

    feature : drag drop tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,23 +3,17 @@ import {
   Flex,
   Text,
   IconButton,
-  Popover,
-  PopoverTrigger,
   Input,
-  PopoverContent,
-  PopoverCloseButton,
-  PopoverBody,
-  PopoverHeader,
-  Button,
   useMediaQuery,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { MdEdit } from "react-icons/md";
+import { Draggable } from "react-beautiful-dnd";
+import PopoverBar from "./PopoverBar";
 import React from "react";
 
-const Task = ({ task, onDelete, onCheck, onRename }) => {
+const Task = ({ task, onDelete, onCheck, onRename, index }) => {
   const [check, setChecked] = useState(task.checked);
-  const [close, setClose] = useState(false);
   const [renameTask, setRenameTask] = useState(task.text);
   const [smallerThan540] = useMediaQuery("(max-width: 540px)");
   let checkBoxProps = {};
@@ -33,77 +27,65 @@ const Task = ({ task, onDelete, onCheck, onRename }) => {
   };
 
   return (
-    <Flex
-      bg={bgColor}
-      _hover={{ bg: "teal.400", borderColor: "teal.400" }}
-      borderWidth={20}
-      maxWidth="25rem"
-      width={smallerThan540 ? "20rem" : "25rem"}
-      rounded={16}
-      borderColor={bgColor}
-      mb={2}
-      color="black"
-    >
-      <Checkbox
-        maxWidth="18rem"
-        width="18rem"
-        onChange={Checked}
-        onDoubleClick={() => onDelete(task.id)}
-        wordBreak="break-word"
-        {...checkBoxProps}
-      >
-        <Text
-          fontSize={17}
-          opacity={check ? 0.5 : 1}
-          textDecor={check ? "line-through" : "none"}
+    <Draggable draggableId={`${task.id}`} index={index}>
+      {provided => (
+        <Flex
+          bg={bgColor}
+          _hover={{ bg: "teal.400", borderColor: "teal.400" }}
+          borderWidth={20}
+          maxWidth="25rem"
+          width={smallerThan540 ? "20rem" : "25rem"}
+          rounded={16}
+          borderColor={bgColor}
+          mb={2}
+          color="black"
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
         >
-          {renameTask.trim() ? renameTask : task.text}
-        </Text>
-      </Checkbox>
-      <Popover closeOnBlur={false} isOpen={close}>
-        <PopoverTrigger>
-          <IconButton
-            justifyContent="flex-end"
-            size="sm"
-            float="right"
-            bgColor="rgb(0, 0, 0, 0.1)"
-            icon={<MdEdit style={{ marginRight: "6.4px" }} />}
-            ml={10}
-            onClick={() => setClose(true)}
-          />
-        </PopoverTrigger>
-        <PopoverContent color="white" bg="blue.800" borderColor="blue.800">
-          <PopoverHeader pt={4} fontWeight="bold" border="0">
-            Rename
-          </PopoverHeader>
-          <PopoverCloseButton onClick={() => setClose(false)} />
-          <PopoverBody>
-            <Input
-              onChange={e => setRenameTask(e.target.value)}
-              defaultValue={task.text}
-              variant="filled"
-              placeholder="rename Task"
-              onKeyDown={e => {
-                if (e.key === "Enter") {
-                  setClose(false);
-                  onRename(task.id, renameTask.trim());
-                }
-              }}
-            />
-            <Button
-              onClick={() => {
-                onRename(task.id, renameTask);
-                setClose(false);
-              }}
-              mt={1}
-              colorScheme="green"
+          <Checkbox
+            maxWidth="18rem"
+            width="18rem"
+            onChange={Checked}
+            onDoubleClick={() => onDelete(task.id)}
+            wordBreak="break-word"
+            {...checkBoxProps}
+          >
+            <Text
+              fontSize={17}
+              opacity={check ? 0.5 : 1}
+              textDecor={check ? "line-through" : "none"}
             >
-              Confirm
-            </Button>
-          </PopoverBody>
-        </PopoverContent>
-      </Popover>
-    </Flex>
+              {renameTask.trim() ? renameTask : task.text}
+            </Text>
+          </Checkbox>
+          <PopoverBar
+            header="Rename"
+            button={
+              <IconButton
+                justifyContent="flex-end"
+                size="sm"
+                float="right"
+                bgColor="rgb(0, 0, 0, 0.1)"
+                icon={<MdEdit style={{ marginRight: "6.4px" }} />}
+                ml={10}
+              />
+            }
+            body={
+              <Input
+                onChange={e => setRenameTask(e.target.value)}
+                defaultValue={task.text}
+                variant="filled"
+                placeholder="Rename task"
+                onKeyDown={e => {
+                  if (e.key === "Enter") onRename(task.id, renameTask.trim());
+                }}
+              />
+            }
+          />
+        </Flex>
+      )}
+    </Draggable>
   );
 };
 
