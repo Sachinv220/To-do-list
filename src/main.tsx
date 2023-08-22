bd214303b225b29610dc5684302c7d7d55f05f70
Sachin <vsachin220@gmail.com>
2023-08-22 16:06:16 +0530

    migrate completely to typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <ChakraProvider>
     <BrowserRouter>
